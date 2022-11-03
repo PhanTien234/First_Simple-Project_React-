63381c4efa22c87d32f92422420906328371eb24
PhanTien234 <minhtienp328@gmail.com>
2022-11-03 15:02:21 +0700

    implement basic todolist app react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Todolist from "./components/Todolist";
+import Textfield from "@atlaskit/textfield"
+import Button from "@atlaskit/button";
+import { useCallback, useEffect, useState } from "react";
+import { v4 } from 'uuid'
 function App() {
+  // state la cac du lieu noi tai cua component hien tai, props la cac du lieu dc truyen tu ben ngoai vao
+  const TODO_APP_STORAGE_KEY = 'TODO_APP';
+  const [todoList, setTodoList]= useState([]); // array
+  const [textInput, setTextInput]= useState(""); // array
+
+  useEffect(() =>{
+    const storagedTodoList = localStorage.getItem(TODO_APP_STORAGE_KEY);
+    if(storagedTodoList){
+      setTodoList(JSON.parse(storagedTodoList));
+      }
+    }, []
+  )
+  useEffect(() => {
+      localStorage.setItem(TODO_APP_STORAGE_KEY, JSON.stringify(todoList));
+    }, [todoList]
+  
+  );
+  const onTextInputChange= useCallback((e) =>{
+    setTextInput(e.target.value);
+  }, []);
+  const onAddBtnClick = useCallback((e)=>{
+    // them text input vao todoList
+      setTodoList([
+        { id: v4(), name: textInput, isCompleted: false},
+        ...todoList, 
+      ]);
+
+    setTextInput("");
+    }, 
+  [textInput, todoList]
+  );
+
+  const onCheckBtnClick = useCallback((id) =>{
+    setTodoList(prevState => 
+      prevState.map((todo) => 
+      todo.id === id ? {...todo, isCompleted:true} : todo
+      )
+    );
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <h3>Danh sach can lam</h3>
+    <Textfield name="add-todo" 
+    placeholder="Them viec can lam..." 
+    elemAfterInput={
+      <Button isDisabled={!textInput} appearance="primary" onClick={onAddBtnClick}>
+        Them
+      </Button>
+    }
+    css={{padding: "2px 4px 2px"}}
+    value={textInput}
+    onChange= {onTextInputChange}
+    ></Textfield>
+    <Todolist todoList={todoList} onCheckBtnClick={onCheckBtnClick}/>
+    </>
   );
 }
 
